9a9cbf4a38a0d62a94b9cec0dae72ac6987eed7c
piupiuhwt <1078407859@qq.com>
2020-11-09 10:01:45 +0800

    1. service init

---
+++ boom_plane_service/src/main/java/com/hwt/BoomPlaneApplication.java
@@ -0,0 +1,11 @@
+package com.hwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomPlaneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoomPlaneApplication.class);
+    }
+}
